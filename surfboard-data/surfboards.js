9362ca4f4dd9120dd1c154dfeedf85120b5b0708
Will G Piro <willgpiro@gmail.com>
2020-01-22 09:51:30 -0800

    Static Design in HTML

---
+++ surfboard-data/surfboards.js
@@ -0,0 +1,66 @@
+const longboard = {
+    id: 'longboard'
+    name: 'Log Longboard',
+    image: 'longboard.png'
+    description: 'Heavy, long, cumbersome - everyones first board, until your bored.'
+    category: 'beginner boards'
+    price: '900.00'
+}
+
+const shortboard = {
+    id: 'shortboard'
+    name:'Snappy Shortboard'
+    image:
+    description: 'Quick, precise, technical - you think your ready, but you\'re not.'
+    category:'advanced boards'
+    price: '700.00'
+}
+
+const fish = {
+    id: 'fish'
+    name: 'Flying Fish'
+    image:
+    description: 'Flowy, free soul, '
+    category:'intermediate - advanced boards'
+    price: '650.00'
+}
+
+const fun_board = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const egg = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const step_up = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+    cost:
+}
+
+const asymmetrical = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+    cost:
+}
+
+export default surfboards.js
