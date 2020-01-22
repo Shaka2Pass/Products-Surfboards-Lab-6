7b65dc51c5fb06c2a22656c5d35f6016e78fa620
Will G Piro <willgpiro@gmail.com>
2020-01-22 10:42:30 -0800

    Product Data Update in JS

--- surfboard-data/surfboards.js
+++ surfboard-data/surfboards.js
@@ -1,66 +1,88 @@
 const longboard = {
-    id: 'longboard'
+    id: 'longboard',
     name: 'Log Longboard',
-    image: 'longboard.png'
-    description: 'Heavy, long, cumbersome - everyones first board, until your bored.'
-    category: 'beginner boards'
-    price: '900.00'
-}
+    image: 'longboard.png',
+    description: 'Heavy, long, cumbersome - everyones first board, until your bored.',
+    color: 'Blue',
+    fin: 'Single-Fin',
+    category: 'Beginner',
+    price: '1000.00',
+};
 
 const shortboard = {
-    id: 'shortboard'
-    name:'Snappy Shortboard'
-    image:
-    description: 'Quick, precise, technical - you think your ready, but you\'re not.'
-    category:'advanced boards'
-    price: '700.00'
-}
+    id: 'shortboard',
+    name:'Snappy Shortboard',
+    image:'',
+    description:'Quick, precise, technical - you think your ready, but you\'re not.',
+    color:  'Red',
+    fin: 'Thruster',
+    category:'Intermediate - Advanced',
+    price: '700.00',
+};
 
 const fish = {
-    id: 'fish'
-    name: 'Flying Fish'
-    image:
-    description: 'Flowy, free soul, '
-    category:'intermediate - advanced boards'
-    price: '650.00'
-}
+    id: 'fish',
+    name: 'Flying Fish',
+    image:'',
+    description: 'Flowy, free soul, retro - you are already surfing better',
+    color: 'Orange',
+    fin: 'Twin-fin',
+    category:'Intermediate - Advanced',
+    price: '650.00',
+};
 
 const fun_board = {
-    id:
-    name:
-    image:
-    description:
-    category:
-    price: 
-}
+    id: 'fun_board',
+    name: 'Funny Funboard',
+    image: '',
+    description: 'Floaty, smooth, forgiving - because catching more waves is never a bad thing.',
+    color: 'Blue',
+    fin: "FCS",
+    category: 'Beginner',
+    price: '900.00',
+};
 
 const egg = {
-    id:
-    name:
-    image:
-    description:
-    category:
-    price: 
+    id: 'egg',
+    name: 'The Estatic Egg',
+    image: '',
+    description: 'Wide, versatile, quiver-kiler - sometimes you need to surf an egg to make a wave.',
+    color: 'Yellow',
+    fin: 'Two Plus One',
+    category: 'Beginner - Advanced',
+    price: '700.00',
 }
 
 const step_up = {
-    id:
-    name:
-    image:
-    description:
-    category:
-    price: 
-    cost:
+    id: 'step_up',
+    name: 'Gun It Step Up',
+    image: '',
+    description: 'Fast, furious, finesse - its big out there, but you are ready, time to Gun It.',
+    color: 'Red',
+    fin: 'Quad',
+    category: 'Advanced',
+    price: '900.00',
 }
 
 const asymmetrical = {
-    id:
-    name:
-    image:
-    description:
-    category:
-    price: 
-    cost:
-}
+    id: 'aysmmetrical',
+    name: 'A-Typical Asym',
+    image: '',
+    description: 'Wierd, wild, works - not sure it should work, but it does.',
+    color: 'pink',
+    fin: 'Two Plus One',
+    category: 'Intermediate - Advanced',
+    price: '750.00',
+};
 
-export default surfboards.js
+const surfboards = [
+    longboard,
+    shortboard,
+    fish,
+    fun_board,
+    egg,
+    step_up,
+    asymmetrical,
+];
+
+export default surfboards.js;
